7e30b83647486f3da22e805fa7927673e93a4526
Anderson-Damen <Andersondamen@gmail.com>
2024-09-19 12:50:06 -0600

    Wrote Prep #2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        int grade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (grade >= 94)
+        {
+            letter = "A";
+        }
+        else if (grade >= 90)
+        {
+            letter = "A-";
+        }
+        else if (grade >= 87)
+        {
+            letter = "B+";
+        }
+        else if (grade >= 84)
+        {
+            letter = "B";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B-";
+        }
+        else if (grade >= 77)
+        {
+            letter = "C+";
+        }
+        else if (grade >= 74)
+        {
+            letter = "C";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C-";
+        }
+        else if (grade >= 67)
+        {
+            letter = "D+";
+        }
+        else if (grade >= 64)
+        {
+            letter = "D";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D-";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you did not pass.");
+        }
+        Console.WriteLine($"Your grade is: {letter}");
     }
 }
